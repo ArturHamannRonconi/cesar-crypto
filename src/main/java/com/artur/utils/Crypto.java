863c3a0e61ac809ebbcac6ab408062ee5b85e50c
ArturHamannRonconi <arturhronconi@gmail.com>
2021-09-20 09:06:14 -0300

    using reduce

--- src/main/java/com/artur/utils/Crypto.java
+++ src/main/java/com/artur/utils/Crypto.java
@@ -1,42 +1,29 @@
 package com.artur.utils;
 
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
 import java.util.function.IntFunction;
 
 public class Crypto {
     private Crypto() {  }
 
     public static String encode(String text) {
-        IntFunction<Character> intToChars =
-        intCharacter -> (char) (intCharacter + 4);
-
-        BiFunction<StringBuilder, Character, StringBuilder> concatenate =
-            (stringBuilder, character) -> stringBuilder.append(character);
-
-        BinaryOperator<StringBuilder> create =
-            (str1, str2) -> str1.append(str2);
+        IntFunction<StringBuilder> addToCharacter =
+            intCharacter -> new StringBuilder()
+                .appendCodePoint(intCharacter + 4);
 
         return text.chars()
-            .mapToObj(intToChars)
-            .reduce(new StringBuilder(), concatenate, create)
+            .mapToObj(addToCharacter)
+            .reduce(new StringBuilder(), StringBuilder::append)
             .toString();
     }
 
     public static String decode(String code) {
-        IntFunction<Character> intToChars =
-        intCharacter -> (char) (intCharacter - 4);
-
-        BiFunction<StringBuilder, Character, StringBuilder> concatenate =
-            (stringBuilder, character) -> stringBuilder.append(character);
-
-        BinaryOperator<StringBuilder> create =
-            (str1, str2) -> str1.append(str2);
+        IntFunction<StringBuilder> addToCharacter =
+            intCharacter -> new StringBuilder()
+                .appendCodePoint(intCharacter - 4);
 
         return code.chars()
-            .mapToObj(intToChars)
-            .reduce(new StringBuilder(), concatenate, create)
+            .mapToObj(addToCharacter)
+            .reduce(new StringBuilder(), StringBuilder::append)
             .toString();
-
     }
 }
